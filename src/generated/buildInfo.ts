8946c0a23e4422483c9190d45896491f6b5c9f0f
강찬욱 <rkdcksdnr1@gmail.com>
2026-04-10 19:03:03 +0900

    chore: 빌드 정보 업데이트
    - buildId, builtAtIso, builtAtLabel 값을 최신으로 수정
    - 앱 버전은 변경되지 않음
    - 빌드 관련 정보의 정확성을 유지하기 위한 작업

--- src/generated/buildInfo.ts
+++ src/generated/buildInfo.ts
@@ -1,6 +1,6 @@
 export const AppBuildInfo = {
   appVersion: "1.0.0",
-  buildId: "20260410-185841",
-  builtAtIso: "2026-04-10T09:58:41.973Z",
-  builtAtLabel: "2026-04-10 18:58:41 KST",
+  buildId: "20260410-190216",
+  builtAtIso: "2026-04-10T10:02:16.863Z",
+  builtAtLabel: "2026-04-10 19:02:16 KST",
 } as const;
